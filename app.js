83ce1cc67e25450a645f9d0ddc47375be01be3c4
Daniel Nagy <Danny@Dannys-MacBook-Pro.local>
2015-06-07 20:16:29 -0400

    fixing spelling in route

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === 'development') {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/nutriton/deserts', deserts);
+app.use('/nutriton/desserts', deserts);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
